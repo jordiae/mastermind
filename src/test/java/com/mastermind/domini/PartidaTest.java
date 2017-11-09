8bc0ce37266544681de97997a820e1f15ea88af5
Jordi Armengol Estapé <me@jordiae.com>
2017-11-09 11:40:11 +0100

    Gradle

---
+++ src/test/java/com/mastermind/domini/PartidaTest.java
@@ -0,0 +1,38 @@
+package com.mastermind.domini;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartidaTest {
+    @org.junit.jupiter.api.Test
+    void getDifficulty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getID() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTaulell() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTime() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isCodeMaker() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isHelp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCodeMaker() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setDifficulty() {
+    }
+
+}
