753a9cc408a62f9d227f06b801a66b739992ea9b
Jordi Armengol Estapé <me@jordiae.com>
2017-11-12 12:11:06 +0100

    Gradle

--- src/test/java/com/mastermind/domini/CodiDriver.java
+++ src/test/java/com/mastermind/domini/CodiDriver.java
@@ -1,4 +1,96 @@
 package com.mastermind.domini;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CodiDriver {
+    private static Codi codi;
+    static boolean constructorExecuted;
+
+    public static void main(String[] args) throws IOException {
+        constructorExecuted = false;
+        boolean runDriver = true;
+        ArrayList<String> availableOptions = new ArrayList<String>();
+        availableOptions.add("0 - Exit driver");
+        availableOptions.add("1 - public Codi(ArrayList<Integer> peces)");
+        availableOptions.add("2 - public ArrayList<Integer> getPeces()");
+        availableOptions.add("3 - public void setPeces(ArrayList<Integer> peces)");
+        availableOptions.add("4 - public ArrayList<Integer> getPeces()");
+        availableOptions.add("5 - public String dataToString()");
+        while (runDriver) {
+            System.out.println("CodiDriver");
+            System.out.println("Mètodes disponibles:");
+            for (String s : availableOptions) {
+                System.out.println(s);
+            }
+            int option = System.in.read();
+            switch(option) {
+                case 0:
+                    runDriver = false;
+                    break;
+                case 1:
+                    runCodi();
+                    break;
+                case 2:
+                    runGetPeces();
+                    break;
+                case 3:
+                    runSetPeces();
+                    break;
+                case 4:
+                    runDataToString();
+                    break;
+            }
+        }
+
+
+    }
+    private static void runCodi() {
+        ArrayList<Integer> p = readNumsFromCommandLine();
+        codi = new Codi(p);
+        constructorExecuted = true;
+
+    }
+
+    private static void runGetPeces() {
+        String s = codi.getPeces().toString();
+        System.out.println(s);
+
+    }
+
+    private static void runSetPeces() {
+        ArrayList<Integer> p = readNumsFromCommandLine();
+        codi.setPeces(p);
+
+    }
+
+    private static void runDataToString() {
+        String s = codi.dataToString();
+        System.out.println(s);
+    }
+
+
+    private static ArrayList<Integer> readNumsFromCommandLine() {
+
+        Scanner s = new Scanner(System.in);
+
+        int count = s.nextInt();
+        s.nextLine(); // throw away the newline.
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>(count);
+        Scanner numScanner = new Scanner(s.nextLine());
+        for (int i = 0; i < count; i++) {
+            if (numScanner.hasNextInt()) {
+                numbers.add (numScanner.nextInt());
+            } else {
+                System.out.println("You didn't provide enough numbers");
+                break;
+            }
+        }
+
+        return numbers;
+    }
+
 }
