23368aacfc783157237aeb08b60b84f179e9d679
carles <carlesbalsellsrodas@hotmail.com>
2017-11-17 00:40:09 +0100

    Interactive IA Driver implemented

--- src/test/java/com/mastermind/domini/IADriver.java
+++ src/test/java/com/mastermind/domini/IADriver.java
@@ -2,68 +2,48 @@ package com.mastermind.domini;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class IADriver {
-    private static IA ia;
-    static boolean constructorExecuted;
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        constructorExecuted = false;
-        boolean runDriver = true;
-        ArrayList<String> availableOptions = new ArrayList<String>();
-        availableOptions.add("0 - Exit driver");
-        availableOptions.add("1 - public IA(int colors, int positions)");
-        availableOptions.add("2 - public Codi nextGuess()");
-        availableOptions.add("3 - public void updateLastGuess(int nBlack, int nWhite)");
-        while (runDriver) {
-            System.out.println("IADriver");
-            System.out.println("Mètodes disponibles:");
-            for (String s : availableOptions) {
-                System.out.println(s);
-            }
-            int option;
-            option = scanner.nextInt();
-            System.out.println(option);
-            switch (option) {
-                case 0:
-                    runDriver = false;
-                    break;
-                case 1:
-                    runIA();
-                    break;
-                case 2:
-                    runNextGuess();
-                    break;
-                case 3:
-                    runUpdateLastGuess();
-                default:
-                    break;
-            }
-        }
 
-    }
+        System.out.println("IADriver");
+        System.out.println("Aquest driver consisteix en un mini-joc interactiu amb la IA," +
+                " jugant com a CodeMaker. \n" + "Escolleix un codi inicial amb 4 colors de l'1 al 6.\n" +
+                "Nota: 4 posicions i 6 colors es correspon a la dificultat fàcil del joc.");
 
-    private static void runIA() {
-        System.out.println("Escriu el nombre de colors i de posicions amb els quals jugarà la IA.");
-        int colors = scanner.nextInt();
-        int posicions = scanner.nextInt();
-        ia = new IA(colors,posicions);
-        constructorExecuted = true;
-        System.out.println("S'ha creat una nova instància de IA.");
+        ArrayList<Integer> codiInicial = new ArrayList<>();
+        int tirades = 0;
+        for (int i = 0; i < 4; ++i) codiInicial.add(scanner.nextInt());
+        IA ia = new IA(6,4);
+        System.out.println("Codi proposat de la IA:");
+        Codi respostaIA = ia.firstGuess();
         System.out.println(ia.firstGuess().dataToString());
+        ++tirades;
+        while (!respostaIA.equals(codiInicial)) {
+            System.out.println("Indica el nombre de peces negres i blanques respectivament.");
+            int pecesNegres = scanner.nextInt();
+            int pecesBlanques = scanner.nextInt();
+            Resposta resposta = new Resposta();
+            resposta.calcularResposta(respostaIA, new Codi(codiInicial));
+            if (resposta.getnBlacks() != pecesNegres || resposta.getnWhites() != pecesBlanques) {
+                System.out.println("Has escrit malament el nombre de peces negres o blanques.");
+                System.out.println("El codi correcte era:");
+                System.out.println(String.valueOf(resposta.getnBlacks()) + " peces negres.");
+                System.out.println(String.valueOf(resposta.getnWhites()) + " peces blanques.");
+                pecesNegres = resposta.getnBlacks();
+                pecesBlanques = resposta.getnWhites();
+            } else System.out.println("Has escrit la resposta correctament!");
+            ia.updateLastGuess(pecesNegres,pecesBlanques);
+            System.out.println("Codi proposat de la IA:");
+            respostaIA = ia.nextGuess();
+            System.out.println(ia.nextGuess().dataToString());
+            ++tirades;
+        }
+        System.out.println("Sembla ser que la IA ha esbrinat el teu codi!\n" +
+                "El joc ha acabat en " + String.valueOf(tirades) + " tirades.");
     }
 
-    private static void runNextGuess() {
-        System.out.println(ia.nextGuess().dataToString());
-    }
-
-    private  static void  runUpdateLastGuess() {
-        System.out.println("Digues el nombre de peces negres i blanques que vols col·locar respectivament");
-        int nBlack = scanner.nextInt();
-        int nWhite = scanner.nextInt();
-        ia.updateLastGuess(nBlack,nWhite);
-    }
 }
