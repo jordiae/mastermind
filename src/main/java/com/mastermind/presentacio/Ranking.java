72361ab7368bf39ca11f9a36c58ef482415fb07c
Urigueler <oriolsaso@gmail.com>
2018-01-04 17:34:41 +0100

    commit
    comiit

--- src/main/java/com/mastermind/presentacio/Ranking.java
+++ src/main/java/com/mastermind/presentacio/Ranking.java
@@ -52,7 +52,52 @@ public class Ranking {
             Record r = rec.get(0);
             textArea2.setText(r.getName() + " " + r.getScore());
         }
-        else {}
+        else {textArea2.setText("No s'ha aconseguit cap record");}
+        if (size >= 2){
+            Record r = rec.get(1);
+            textArea3.setText(r.getName() + " " + r.getScore());
+        }
+        else {textArea3.setText("No s'ha aconseguit cap record");}
+        if (size >= 3){
+            Record r = rec.get(2);
+            textArea4.setText(r.getName() + " " + r.getScore());
+        }
+        else {textArea4.setText("No s'ha aconseguit cap record");}
+        if (size >= 4){
+            Record r = rec.get(3);
+            textArea5.setText(r.getName() + " " + r.getScore());
+        }
+        else {textArea5.setText("No s'ha aconseguit cap record");}
+        if (size >= 5){
+            Record r = rec.get(4);
+            textArea6.setText(r.getName() + " " + r.getScore());
+        }
+        else {textArea6.setText("No s'ha aconseguit cap record");}
+        if (size >= 6){
+            Record r = rec.get(5);
+            textArea7.setText(r.getName() + " " + r.getScore());
+        }
+        else {textArea7.setText("No s'ha aconseguit cap record");}
+        if (size >= 7){
+            Record r = rec.get(6);
+            textArea8.setText(r.getName() + " " + r.getScore());
+        }
+        else {textArea8.setText("No s'ha aconseguit cap record");}
+        if (size >= 8){
+            Record r = rec.get(7);
+            textArea9.setText(r.getName() + " " + r.getScore());
+        }
+        else {textArea9.setText("No s'ha aconseguit cap record");}
+        if (size >= 9){
+            Record r = rec.get(8);
+            textArea10.setText(r.getName() + " " + r.getScore());
+        }
+        else {textArea10.setText("No s'ha aconseguit cap record");}
+        if (size >= 10){
+            Record r = rec.get(9);
+            textArea11.setText(r.getName() + " " + r.getScore());
+        }
+        else {textArea11.setText("No s'ha aconseguit cap record");}
 
     }
 
