621aa4669264240eb678169dafd62aa818f394e3
Jordi Armengol Estapé <me@jordiae.com>
2018-01-04 18:48:30 +0100

    Update ControladorPartida.java

--- src/main/java/com/mastermind/domini/ControladorPartida.java
+++ src/main/java/com/mastermind/domini/ControladorPartida.java
@@ -4,6 +4,7 @@ package com.mastermind.domini;
 
 import com.mastermind.persistencia.DataController;
 
+import javax.xml.crypto.Data;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,17 +15,32 @@ public class ControladorPartida {
     private IA ia;
     private boolean partidaCarregadaCorrectament;
     private int N_COLORS = 6; // Nota: mai no hem provat amb una mida diferent de 6
+    private Codi iaFirstGuess;
+    private boolean codeMakerTurn;
+    long initTime;
+    String user;
+
     // Constructora nova partida
-    public ControladorPartida(int difficulty, boolean codeMaker) {
+    public ControladorPartida(int difficulty, boolean codeMaker, Codi codiSolució, String username) {
+        user = username;
         int id = (int) (new Date().getTime()/1000);
         boolean help = false;
         Time time = new Time(0);
-        Taulell taulell = createTaulell(difficulty);
+        Taulell taulell = createTaulell(difficulty, codeMaker, codiSolució);
         partida = new Partida(id, difficulty, codeMaker, help, time, taulell);
         int nColors = N_COLORS;
         int mida = sizeByDifficulty(difficulty);
         ia = new IA(nColors,mida);
+        codeMakerTurn = false;
+        if (codeMaker) {
+            iaFirstGuess = ia.firstGuess();
+            codeMakerTurn = true;
+        }
+        initTime = System.nanoTime();
+
     }
+
+
 
 
     // Constructora carregar partida
@@ -33,18 +49,38 @@ public class ControladorPartida {
         if (p != null) {
             partida = p;
             partidaCarregadaCorrectament = true;
+            int nColors = N_COLORS;
+            int mida = sizeByDifficulty(partida.getDifficulty());
+            ia = new IA(nColors,mida,partida.getTaulell().getTirades());
         }
         else {
             partidaCarregadaCorrectament = false;
         }
-        int nColors = N_COLORS;
-        int mida = sizeByDifficulty(partida.getDifficulty());
-        ia = new IA(nColors,mida);
 
     }
     // Aquest metode s'ha de cridar justament despres de cridar la Constructora de arregar partida
     public boolean comprovarPartidaCarregada() {
         return partidaCarregadaCorrectament;
+    }
+    private long getCurrentElapsedTime() {
+        return System.nanoTime() - initTime;
+    }
+
+    public boolean desaPartida() {
+        partida.setTime(new Time(this.getCurrentElapsedTime()));
+        if (DataController.savePartida(partida,user))
+            return true;
+        else {
+            return false;
+        }
+    }
+
+    public Codi nextGuessIA() {
+        /*if (nova || partida.getTaulell().getTorn() == 0) {}
+        else {
+            return ia.nextGuess();
+        }*/
+        return ia.nextGuess();
     }
 
     // Nota: mai no hem provat amb una mida diferent de 4
@@ -53,22 +89,35 @@ public class ControladorPartida {
         return mida;
     }
 
-    public Taulell novaTiradaCodebreaker(Codi codi) {
+    public Taulell novaTiradaUsuariCodebreaker(Codi codi) {
         Taulell taulell = partida.getTaulell();
-        taulell.ferTirada(codi);
-        partida.setTaulell(taulell);
-        return taulell;
+        boolean tiradaValida = taulell.ferTirada(codi);
+        if (tiradaValida && !codeMakerTurn) {
+            partida.setTaulell(taulell);
+            return taulell;
+        }
+        else {
+            return null; // retorna null si ens hem passat de torns o no era el seu torn
+        }
     }
 
-    private Taulell createTaulell(int difficulty) {
+
+
+    private Taulell createTaulell(int difficulty, boolean codeMaker, Codi codiSolucio) {
         int mida = sizeByDifficulty(difficulty);
         ArrayList<Integer> peces = new ArrayList<>(mida);
         Random random = new Random();
         int nColors = N_COLORS;
-        for (Integer peca : peces) {
-            peces.add(peca,random.nextInt(nColors));
+        Codi codiSol;
+        if (codeMaker) {
+            codiSol = codiSolucio;
         }
-        Codi codiSolucio = new Codi(peces);
+        else {
+            for (Integer peca : peces) {
+                peces.add(peca, random.nextInt(nColors));
+            }
+            codiSol = new Codi(peces);
+        }
         int torn = 0;
         ArrayList<Tirada> tirades = new ArrayList<>();
         int maxTorn = 10;
