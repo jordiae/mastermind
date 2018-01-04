29dde26b39cf3a493b8efedb71e134935924b040
Jordi Armengol Estapé <me@jordiae.com>
2018-01-04 12:26:11 +0100

    Controladors

--- src/main/java/com/mastermind/domini/ControladorRanking.java
+++ src/main/java/com/mastermind/domini/ControladorRanking.java
@@ -1,10 +1,12 @@
 package com.mastermind.domini;
 
+import com.mastermind.persistencia.DataController;
+
 import java.util.ArrayList;
 
 public class ControladorRanking {
 
     public ArrayList<String> carregaInfoRecords() {
-
+        return DataController.getRecords();
     }
 }
